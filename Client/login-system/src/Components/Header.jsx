8299de3553b8d939825766c16029bbd7fd07d715
zackd44 <zackdavison1@outlook.com>
2024-09-14 14:51:40 +0100

    Refactored display username on login

--- Client/login-system/src/Components/Header.jsx
+++ Client/login-system/src/Components/Header.jsx
@@ -4,17 +4,15 @@ const { useNavigate, useLocation } = require("react-router-dom");
 export default function Header() {
   // create navigate hook
   const navigate = useNavigate();
-  const location = useLocation();
 
   //   retrive state from the location
-  const { username } = location.state || "";
+  //   const { username } = location.state || "";
   return (
     <div>
       <header>
         <h1>Login System</h1>
         <button onClick={() => navigate("/")}>Back</button>
       </header>
-      <h1>Hello {username}</h1>
     </div>
   );
 }
--- Client/login-system/src/Components/LoggedInUserScreen.jsx
+++ Client/login-system/src/Components/LoggedInUserScreen.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import Header from "./Header";
-
+const { useNavigate, useLocation } = require("react-router-dom");
 export default function LoggedInUserScreen() {
+  // create navigate hook
+
+  const location = useLocation();
+
+  //   retrive state from the location
+  const { username } = location.state || "";
+
   return (
     <div>
       <Header />
+      <h1>Hello {username}</h1>
+
+      <form action="#" id="createUserForm">
+        <input type="text" placeholder="Username" />
+        <br />
+        <input type="password" placeholder="Password" />
+      </form>
     </div>
   );
 }
