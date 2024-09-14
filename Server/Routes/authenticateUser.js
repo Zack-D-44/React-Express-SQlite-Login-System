5d412b44b62bf700302ca41ff233f8a6c7985985
zackd44 <zackdavison1@outlook.com>
2024-09-14 13:53:06 +0100

    Refactored code

--- Server/Routes/authenticateUser.js
+++ Server/Routes/authenticateUser.js
@@ -5,9 +5,8 @@ const authenticateUserRouter = express.Router();
 // In this request, username and password are sent in the body
 authenticateUserRouter.post("/", async (req, res) => {
   try {
-    const username = req.body.username;
-    const password = req.body.password;
-    console.log(req.body);
+    const { username, password } = req.body;
+
     // Ensure that authenticateUser is not redefined
     const isAuthenticated = await authenticateUser(username, password); // Assume authenticateUser is async
 
