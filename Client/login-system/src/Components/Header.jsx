5e469de7e11625843a702946d82907542cc0be94
zackd44 <zackdavison1@outlook.com>
2024-09-14 16:05:58 +0100

    Styled header

--- Client/login-system/src/Components/Header.jsx
+++ Client/login-system/src/Components/Header.jsx
@@ -1,17 +1,25 @@
 import React from "react";
+import styles from "../styles/header.module.css";
+import globalStyles from "../styles/globalStyles.module.css";
 const { useNavigate, useLocation } = require("react-router-dom");
 
 export default function Header() {
   // create navigate hook
   const navigate = useNavigate();
+  const location = useLocation();
 
   //   retrive state from the location
   //   const { username } = location.state || "";
+  //   retrive state from the location
+  const { username } = location.state || "";
   return (
     <div>
-      <header>
+      <header className={styles.header}>
         <h1>Login System</h1>
-        <button onClick={() => navigate("/")}>Back</button>
+        <h1 className={styles.greetingHeading}>Hello {username}</h1>
+        <button className={globalStyles.button} onClick={() => navigate("/")}>
+          &#8592; Back
+        </button>
       </header>
     </div>
   );
--- Client/login-system/src/Components/LoggedInUserScreen.jsx
+++ Client/login-system/src/Components/LoggedInUserScreen.jsx
@@ -1,23 +1,19 @@
 import React from "react";
 import Header from "./Header";
-const { useNavigate, useLocation } = require("react-router-dom");
+// const { useNavigate, useLocation } = require("react-router-dom");
 export default function LoggedInUserScreen() {
   // create navigate hook
-
-  const location = useLocation();
-
-  //   retrive state from the location
-  const { username } = location.state || "";
 
   return (
     <div>
       <Header />
-      <h1>Hello {username}</h1>
 
       <form action="#" id="createUserForm">
         <input type="text" placeholder="Username" />
         <br />
         <input type="password" placeholder="Password" />
+        <br />
+        <input type="submit" value="Create User" />
       </form>
     </div>
   );
