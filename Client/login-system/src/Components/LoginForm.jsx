f52529a0ddc474e53a83c7c864177bd0f30723b1
zackd44 <zackdavison1@outlook.com>
2024-09-14 13:21:58 +0100

    Configured CORS properly

--- Client/login-system/src/Components/LoginForm.jsx
+++ Client/login-system/src/Components/LoginForm.jsx
@@ -1,50 +1,61 @@
-import React from 'react'
-import styles from '../styles/loginForm.module.css'
-import { useState } from 'react';
+import React from "react";
+import styles from "../styles/loginForm.module.css";
+import { useState } from "react";
 
 export default function LoginForm() {
   // let userValid = false;
   // states for password and user name
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  
-  async function authenticateUser(event){
-    const url = 'http://localhost:4023/authenticateUser/';
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function authenticateUser(event) {
+    const url = "http://localhost:4023/authenticateUser/";
 
     event.preventDefault();
     // console.log(username, password);
-    try{
-      const response = await fetch(url, {method: 'POST', body: JSON.stringify({"username": username, "password": password}), headers: { 'Content-Type': 'application/json'}, mode: 'cors'});
-      
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify({ username: username, password: password }),
+        headers: { "Content-Type": "application/json" },
+      });
 
-
-      if(response.ok){
+      if (response.ok) {
         const json = await response.json();
-        console.log(json);        
+        console.log(json);
       }
-
-      
-
-
-    }catch(error){
+    } catch (error) {
       console.log(error);
     }
-  };
-    
-
+  }
 
   return (
-    <div className={`${styles.centreVerticallyAndHorizontally} ${styles.font} ${styles.formContainer}`}>
-
-        <h2 className={styles.headingStyle}>Login</h2>
-        {/* Login user form */}
-        <form action="#" onSubmit={authenticateUser}>
-            <input type="text" placeholder="Username" className={styles.formInputs} value={username} onChange={e => setUsername(e.target.value)}/>
-            <br />
-            <input type="password" placeholder="Password" className={styles.formInputs} value={password} onChange={e => {setPassword(e.target.value)}}/>
-            <br />
-            <input type="submit" value="Login" className={styles.loginButton} />
-        </form>
+    <div
+      className={`${styles.centreVerticallyAndHorizontally} ${styles.font} ${styles.formContainer}`}
+    >
+      <h2 className={styles.headingStyle}>Login</h2>
+      {/* Login user form */}
+      <form action="#" onSubmit={authenticateUser}>
+        <input
+          type="text"
+          placeholder="Username"
+          className={styles.formInputs}
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <br />
+        <input
+          type="password"
+          placeholder="Password"
+          className={styles.formInputs}
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <br />
+        <input type="submit" value="Login" className={styles.loginButton} />
+      </form>
     </div>
-  )
+  );
 }
--- Server/server.js
+++ Server/server.js
@@ -6,7 +6,20 @@ const authenticateUserRouter = require("./Routes/authenticateUser");
 // create express app
 const app = express();
 
+// Import CORS
+const cors = require("cors");
+
 app.use(express.json());
+
+app.use(
+  cors({
+    // can use origin true to allow all domains
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    credentials: false,
+    // credentials specifies whether cookies are allowed in the response
+  })
+);
 
 app.use("/authenticateUser", authenticateUserRouter);
 
